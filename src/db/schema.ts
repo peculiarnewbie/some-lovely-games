af99374b33a51053df702672b57ce718800894c5
Arif Rahman (Bolt) <32804415+peculiarnewbie@users.noreply.github.com>
2024-08-10 21:13:33 +0700

    add game and player to schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const userTable = sqliteTable("user", {
@@ -21,4 +22,27 @@ export const gameTable = sqliteTable("game", {
 	name: text("name").notNull(),
 	createdAt: integer("created_at").notNull(),
 	type: text("type").notNull(),
+	round: integer("round").notNull().default(0),
+	leader: text("leader")
+		.notNull()
+		.references(() => userTable.id),
 });
+
+export const gameRelations = relations(gameTable, ({ many }) => ({
+	players: many(userTable),
+}));
+
+export const playerTable = sqliteTable("player", {
+	id: text("id")
+		.notNull()
+		.references(() => userTable.id)
+		.primaryKey(),
+	gameId: text("game_id"),
+});
+
+export const playerRelations = relations(playerTable, ({ one }) => ({
+	author: one(gameTable, {
+		fields: [playerTable.gameId],
+		references: [gameTable.id],
+	}),
+}));
