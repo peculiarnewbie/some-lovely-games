53bded6bbf197d6ea1a6d8005f0d7b4e48e0acb6
Arif Rahman (Bolt) <32804415+peculiarnewbie@users.noreply.github.com>
2024-10-13 12:25:56 +0700

    worker w/ static assets

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
-	server: {
-		preset: "cloudflare-pages",
-		// rollupConfig: {
-		// 	external: ["node:async_hooks"],
-		// },
-	},
+  server: {
+    preset: "./cloudflare-pages",
+
+    output: {
+      dir: "{{ rootDir }}/dist",
+      publicDir: "{{ output.dir }}/public",
+      serverDir: "{{ output.dir }}/worker"
+    },
+
+    rollupConfig: {
+      external: ["node:async_hooks"]
+    },
+
+    hooks: {
+      compiled() {}
+    }
+  }
 });
